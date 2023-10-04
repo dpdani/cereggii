9a5fc0834eab8f4d87e1eccec34e5f955163e478
dpdani <git@danieleparmeggiani.me>
2023-10-04 11:34:44 +0200

    ci

--- src/cereggii/__init__.py
+++ src/cereggii/__init__.py
@@ -16,5 +16,6 @@ from .atomic_dict import AtomicDict  # noqa: F401
 from .atomic_ref import AtomicRef  # noqa: F401
 
 if not getattr(sys.flags, "nogil", False):
-    warnings.warn("this library is meant to be used with nogil python: "
-                  "https://github.com/colesbury/nogil", stacklevel=1)
+    warnings.warn(
+        "this library is meant to be used with nogil python: " "https://github.com/colesbury/nogil", stacklevel=1
+    )
