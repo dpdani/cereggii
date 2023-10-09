663b852bee7bb11ce2c99b69754168e42d788426
dpdani <git@danieleparmeggiani.me>
2023-10-09 10:55:53 +0200

    add C AtomicDictType, and point cereggii.AtomicDict to it

--- src/cereggii/atomic_dict/atomic_dict.c
+++ src/cereggii/atomic_dict/atomic_dict.c
@@ -6,22 +6,22 @@
 #define CACHE_LINE_SIZE 64 // in bytes
 
 
-typedef struct CereggiiDictEntry {
+typedef struct {
     char flags;
     Py_hash_t hash;
     PyObject *key;
     PyObject *value;
-} CereggiiDictEntry;
+} AtomicDictEntry;
 
 
-typedef struct CereggiiDictBlock {
+typedef struct {
     PyObject *generation;
 //    PyObject *iteration;
 
-    CereggiiDictEntry entries[64];
-} CereggiiDictBlock;
+    AtomicDictEntry entries[64];
+} AtomicDictBlock;
 
-typedef struct CereggiiDictMeta {
+typedef struct {
     PyObject_HEAD
 
     unsigned int log_size;
@@ -32,54 +32,74 @@ typedef struct CereggiiDictMeta {
     unsigned long index_mask;
     unsigned long distance_mask;
     unsigned long tag_mask;
-} CereggiiDictMeta;
+} AtomicDictMeta;
 
-typedef struct CereggiiDict {
+typedef struct {
     PyObject_HEAD
 
-    CereggiiDictMeta metadata;
-    CereggiiDictBlock *blocks;
-} CereggiiDict;
+    AtomicDictMeta metadata;
+    AtomicDictBlock *blocks;
+} AtomicDict;
 
-PyObject*
-atomic_dict_lookup(CereggiiDict *dict, PyObject *key)
+static PyTypeObject AtomicDictType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "cereggii.AtomicDict",
+    .tp_doc = PyDoc_STR("A thread-safe dictionary (hashmap), that's almost-lock-free™."),
+    .tp_basicsize = sizeof(AtomicDict),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_new = PyType_GenericNew,
+};
+
+PyObject *
+atomic_dict_lookup(AtomicDict *dk, PyObject *key)
 {
     PyErr_SetObject(PyExc_KeyError, key);
     return NULL;
 }
 
-PyObject*
+PyObject *
 atomic_dict_lookup_call(PyObject *self, PyObject *args)
 {
-    PyObject *dict, *key;
+    PyObject *dk, *key;
 
-    if (!PyArg_ParseTuple(args, "OO", &dict, &key))
+    if (!PyArg_ParseTuple(args, "OO", &dk, &key))
         return NULL;
 
-    return atomic_dict_lookup(dict, key);
+    return atomic_dict_lookup(dk, key);
 }
 
 
 static PyMethodDef AtomicDictMethods[] = {
-        {"lookup", atomic_dict_lookup_call, METH_VARARGS, NULL},
-        {NULL, NULL, 0, NULL}
+    {"lookup", atomic_dict_lookup_call, METH_VARARGS, NULL},
+    {NULL, NULL, 0,                                   NULL}
 };
 
-static struct PyModuleDef atomic_dict_module = {
-        PyModuleDef_HEAD_INIT,
-        "_atomic_dict",
-        NULL,
-        -1,
-        AtomicDictMethods,
+static PyModuleDef atomic_dict_module = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "_atomic_dict",
+    .m_doc = NULL,
+    .m_size = -1,
 };
 
 __attribute__((unused)) PyMODINIT_FUNC
-PyInit__atomic_dict(void) {
+PyInit__atomic_dict(void)
+{
     PyObject *m;
+
+    if (PyType_Ready(&AtomicDictType) < 0)
+        return NULL;
 
     m = PyModule_Create(&atomic_dict_module);
     if (m == NULL)
         return NULL;
+
+    Py_INCREF(&AtomicDictType);
+    if (PyModule_AddObject(m, "AtomicDict", (PyObject *) &AtomicDictType) < 0) {
+        Py_DECREF(&AtomicDictType);
+        Py_DECREF(m);
+        return NULL;
+    }
 
     return m;
 }
