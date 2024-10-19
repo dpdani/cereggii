d2526b4fca27ce90c9493ac98e7dbb95638d4fe9
dpdani <git@danieleparmeggiani.me>
2024-10-19 15:23:25 +0200

    ruff upgrade

--- tests/test_atomic_dict.py
+++ tests/test_atomic_dict.py
@@ -49,7 +49,7 @@ def test_weird_init():
 def test_debug():
     d = AtomicDict({"key": "value"}, min_size=64, iterable={1: 0})
     dbg = d.debug()
-    assert type(dbg) is dict  # noqa: E721
+    assert type(dbg) is dict
     assert "meta" in dbg
     assert "index" in dbg
     assert "blocks" in dbg
