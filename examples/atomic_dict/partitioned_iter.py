124abb43a3b07e3b64090635258b9bf4fac38d8f
dpdani <git@danieleparmeggiani.me>
2024-10-25 16:14:51 +0200

    changed examples/atomic_dict/partitioned_iter

--- examples/atomic_dict/partitioned_iter.py
+++ examples/atomic_dict/partitioned_iter.py
@@ -4,19 +4,19 @@ import time
 import cereggii
 
 
-keys = 2**26
+keys = 2**23
 py_d = {}
 
 started = time.time()
 for _ in range(keys):
     py_d[_] = _
-print(f"Insertion into builtin dict took {time.time() - started:.2f}s")
+print(f"Insertion into builtin dict took {(time.time() - started) * 1000:.0f}ms")
 
 started = time.time()
-current = 0
+total = 0
 for k, v in py_d.items():
-    current = k + v
-print(f"Builtin dict iter took {time.time() - started:.2f}s with 1 thread.")
+    total += v
+print(f"Builtin dict iter took {(time.time() - started) * 1000:.0f}ms with 1 thread ({total=}).")
 print("----------")
 del py_d
 
@@ -25,20 +25,23 @@ d = cereggii.AtomicDict(min_size=keys * 2)
 started = time.time()
 for _ in range(keys):
     d[_] = _
-print(f"Insertion took {time.time() - started:.2f}s")
+print(f"Insertion took {(time.time() - started) * 1000:.0f}ms")
 
 for n in range(1, 4):
 
+    partials = [0 for _ in range(n)]
+
     def iterator(i):
         current = 0
-        for k, v in d.fast_iter(partitions=n, this_partition=i):  # noqa: B023
-            current = k + v  # noqa
+        for k, v in d.fast_iter(partitions=n, this_partition=i):
+            current += v
+        partials[i] = current
 
-    threads = [threading.Thread(target=iterator, args=(_,)) for _ in range(1, n + 1)]
+    threads = [threading.Thread(target=iterator, args=(_,)) for _ in range(n)]
 
     started = time.time()
     for t in threads:
         t.start()
     for t in threads:
         t.join()
-    print(f"Partitioned iter took {time.time() - started:.2f}s with {n} threads.")
+    print(f"Partitioned iter took {(time.time() - started) * 1000:.0f}ms with {n} threads ({partials=} {sum(partials)=}).")
