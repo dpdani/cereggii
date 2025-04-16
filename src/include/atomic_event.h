d97495868084c1515eb5571cd9552be6f0af9f22
dpdani <git@danieleparmeggiani.me>
2025-04-16 20:55:16 +0200

    AtomicEvent now supports windows

--- src/include/atomic_event.h
+++ src/include/atomic_event.h
@@ -6,16 +6,21 @@
 #define CEREGGII_ATOMIC_EVENT_H
 
 #include <Python.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+#endif
 
-typedef struct AtomicEvent {
+typedef struct {
     PyObject_HEAD
-
+#ifdef _WIN32
+    HANDLE event;
+#else
     pthread_mutex_t mutex;
     pthread_cond_t cond;
     int state;
+#endif
 } AtomicEvent;
 
 PyObject *AtomicEvent_new(PyTypeObject *type, PyObject *Py_UNUSED(args), PyObject *Py_UNUSED(kwds));
