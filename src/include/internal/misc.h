665531574d020245b648c02a59f2b6014020d5b1
dpdani <git@danieleparmeggiani.me>
2025-04-21 20:14:36 +0200

    self-review

--- src/include/internal/misc.h
+++ src/include/internal/misc.h
@@ -6,19 +6,19 @@
 #define CEREGGII_MISC_H
 
 #if defined(_MSC_VER)
-    #define CEREGGII_ALIGN(x) __declspec(align(x))
+#  define CEREGGII_ALIGN(x) __declspec(align(x))
 #elif defined(__GNUC__) || defined(__clang__)
-    #define CEREGGII_ALIGN(x) __attribute__((aligned(x)))
+#  define CEREGGII_ALIGN(x) __attribute__((aligned(x)))
 #else
-    #define CEREGGII_ALIGN(x) alignas(x)  // C++11 standard
+#  define CEREGGII_ALIGN(x) alignas(x)  // C++11 standard
 #endif
 
 #if defined(_MSC_VER)
-    #define CEREGGII_UNUSED __pragma(warning(suppress: 4100))
+#  define CEREGGII_UNUSED __pragma(warning(suppress: 4100))
 #elif defined(__GNUC__) || defined(__clang__)
-    #define CEREGGII_UNUSED __attribute__((unused))
+#  define CEREGGII_UNUSED __attribute__((unused))
 #else
-    #define CEREGGII_UNUSED
+#  define CEREGGII_UNUSED
 #endif
 
 #endif // CEREGGII_MISC_H
