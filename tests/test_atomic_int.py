3cee85b659e2a052b8763a9ebb0e74bf3ad1f64e
dpdani <git@danieleparmeggiani.me>
2026-01-03 17:27:49 +0100

    fuff

--- tests/test_atomic_int.py
+++ tests/test_atomic_int.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import gc
 from fractions import Fraction
 
 import cereggii
--- tests/test_atomic_ref.py
+++ tests/test_atomic_ref.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present dpdani <git@danieleparmeggiani.me>
 #
 # SPDX-License-Identifier: Apache-2.0
-import gc
 
 import cereggii
 from cereggii import AtomicRef
