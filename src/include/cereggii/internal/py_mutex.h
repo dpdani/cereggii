1bf61ee9f06123e49cfe562f094677e3471bcc61
dpdani <git@danieleparmeggiani.me>
2026-02-11 17:19:55 +0100

    missing header

---
+++ src/include/cereggii/internal/py_mutex.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2026-present dpdani <git@danieleparmeggiani.me>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+// This header contains a compatibility shim for PyMutex to make its usages
+// become no-ops in versions of Python that don't support it.
+// This is fine because usages of PyMutex in this library protect shared state
+// that would otherwise be protected by the GIL.
+
+
+#ifndef CEREGGII_PY_MUTEX_H
+#define CEREGGII_PY_MUTEX_H
+
+#ifndef Py_GIL_DISABLED
+
+#define PyMutex void*
+#define PyMutex_Lock(mutex) do { (void)(mutex); } while (0)
+#define PyMutex_Unlock(mutex) do { (void)(mutex); } while (0)
+
+#endif
+
+#endif //CEREGGII_PY_MUTEX_H
